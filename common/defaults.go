4fd843fb99e50493919432306b650d1425c99139
Julian Yap <julianokyap@gmail.com>
2016-08-10 10:38:43 -1000

    Modify DefaultDataDir

--- common/defaults.go
+++ common/defaults.go
@@ -36,11 +36,11 @@ func DefaultDataDir() string {
 	home := HomeDir()
 	if home != "" {
 		if runtime.GOOS == "darwin" {
-			return filepath.Join(home, "Library", "Ethereum")
+			return filepath.Join(home, "Library", "JumbucksEE")
 		} else if runtime.GOOS == "windows" {
-			return filepath.Join(home, "AppData", "Roaming", "Ethereum")
+			return filepath.Join(home, "AppData", "Roaming", "JumbucksEE")
 		} else {
-			return filepath.Join(home, ".ethereum")
+			return filepath.Join(home, ".jumbucksee")
 		}
 	}
 	// As we cannot guess a stable location, return empty and handle later
