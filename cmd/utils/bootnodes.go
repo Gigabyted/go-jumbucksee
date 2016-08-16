a5b61538f945cf410d2c87e148a4db55a9cb1d47
Julian Yap <julianokyap@gmail.com>
2016-08-16 10:27:53 -1000

    Modify discovery port

--- cmd/utils/bootnodes.go
+++ cmd/utils/bootnodes.go
@@ -22,12 +22,7 @@ import "github.com/jumbucks/go-jumbucksee/p2p/discover"
 // the Frontier network.
 var FrontierBootNodes = []*discover.Node{
 	// ETH/DEV Go Bootnodes
-	discover.MustParseNode("enode://a979fb575495b8d6db44f750317d0f4622bf4c2aa3365d6af7c284339968eef29b69ad0dce72a4d8db5ebb4968de0e3bec910127f134779fbcb0cb6d3331163c@52.16.188.185:30303"), // IE
-	discover.MustParseNode("enode://de471bccee3d042261d52e9bff31458daecc406142b401d4cd848f677479f73104b9fdeb090af9583d3391b7f10cb2ba9e26865dd5fca4fcdc0fb1e3b723c786@54.94.239.50:30303"),  // BR
-	discover.MustParseNode("enode://1118980bf48b0a3640bdba04e0fe78b1add18e1cd99bf22d53daac1fd9972ad650df52176e7c7d89d1114cfef2bc23a2959aa54998a46afcf7d91809f0855082@52.74.57.123:30303"),  // SG
-
-	// ETH/DEV Cpp Bootnodes
-	discover.MustParseNode("enode://979b7fa28feeb35a4741660a16076f1943202cb72b6af70d327f053e248bab9ba81760f39d0701ef1d8f89cc1fbd2cacba0710a12cd5314d5e0c9021aa3637f9@5.1.83.226:30303"),
+	discover.MustParseNode("enode://13e07d637d170fdf1f87edd3d671c1417acb856045c1920c4c0bca1d9c19be2a846af891d2c92a677cbf66b61586426c7fff8f4247b6912d88a7dbb762e26288@107.191.104.192:30388"),
 }
 
 // TestNetBootNodes are the enode URLs of the P2P bootstrap nodes running on the
